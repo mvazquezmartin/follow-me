78f94c1687c2723bc98d5c9eb397ccae805f11e7
mvazquez <matias.vazquezmartin@gmail.com>
2024-04-14 20:11:18 -0300

    fix: jsNota volumen

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ function jsNota(frecuencia) {
   o.frequency.value = frecuencia;
   g.connect(context.destination);
   o.start(0);
-  g.gain.value = 0.4
+  g.gain.value = 0.5
   g.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 1.5);
   return o
 }
